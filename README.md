d3c2bce4c3964760cd8eaa10172712ea70209e4b
Jiasi Chen <jiasi.chen@sailthru.com>
2018-10-23 18:08:43 -0400

    master: change into spark project..

--- README.md
+++ README.md
@@ -1,38 +1,4 @@
-# Flink Tutorial
+# Spark Tutorial
 
-Instructions:
-
-To run KafkaStreamingJob,
-first start kafka zookeeper server and broker
-`cd ~/dev/kafka_2.11-2.0.0/`
-`./bin/zookeeper-server-start.sh ./config/zookeeper.properties`
-`./bin/kafka-server-start.sh ./config/server.properties`
-
-create topic:
-`./bin/kafka-topics.sh --create --topic test --zookeeper localhost:2181 --partitions 1 --replication-factor 1`
-
-verify it works by the console:
-`./bin/kafka-console-producer.sh --topic test --broker-list localhost:9092`
-`./bin/kafka-console-consumer.sh --topic test --bootstrap-server localhost:9092`
-
-run kafkaStreamingJob with program argument:
-`--topic test --bootstrap.servers localhost:9092 --zookeeper.connect localhost:2181 --group.id myGroup`
-produce something in producer console, verify console prints the expected aggregation.
-
-
-
-
-
-
-
-Source: \n
-Basic batch/stream processing
-https://github.com/apache/flink/tree/master/flink-examples/flink-examples-streaming/src
-
-Flink with Kafka:
-https://data-artisans.com/blog/kafka-flink-a-practical-how-to
-
-Exactly once kafka consuming:
-https://flink.apache.org/features/2018/03/01/end-to-end-exactly-once-apache-flink.html
 
 
---
+++ src/main/java/org/myorg/spark/WordCountStreamingJob.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.myorg.spark;
+import org.apache.spark.SparkConf;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
+import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import scala.Tuple2;
+
+import java.util.Arrays;
+//https://github.com/apache/spark/blob/v2.2.0/examples/src/main/java/org/apache/spark/examples/streaming/JavaNetworkWordCount.java
+public final class WordCountStreamingJob {
+    public static void main(String[] args) throws Exception {
+        // Create a local StreamingContext with two working thread and batch interval of 1 second
+        SparkConf conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount");
+        JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(1));
+
+        // Create a DStream that will connect to hostname:port, like localhost:9999
+        JavaReceiverInputDStream<String> lines = jssc.socketTextStream("localhost", 9999);
+
+        // Split each line into words
+        JavaDStream<String> words = lines.flatMap(x -> Arrays.asList(x.split(" ")).iterator());
+
+        // Count each word in each batch
+        JavaPairDStream<String, Integer> pairs = words.mapToPair(s -> new Tuple2<>(s, 1));
+        JavaPairDStream<String, Integer> wordCounts = pairs.reduceByKey((i1, i2) -> i1 + i2);
+
+        // Print the first ten elements of each RDD generated in this DStream to the console
+        wordCounts.print();
+
+        jssc.start();              // Start the computation
+        jssc.awaitTermination();   // Wait for the computation to terminate
+    }
+}
